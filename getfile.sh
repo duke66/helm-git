a1f0882bc60bfb4356fbead3caed5dc044d0245f
Andrey Andreev <andrey.andreev@corp.mail.ru>
2019-02-27 13:48:32 +0300

    try to user custom url with https 4 debug

--- getfile.sh
+++ getfile.sh
@@ -10,7 +10,7 @@ REPO=$(echo $URI | cut -d\@ -f2 | cut -d\/ -f2,3,4,5 | cut -d: -f1)
 BRANCH=$(echo $URI | cut -d\/ -f5| cut -d: -f2)
 FILEPATH=$(echo $URI | cut -d\/ -f6,7,8,9,10,11,12,13 | sed -e "s/$BRANCH\///") # eg: kubernetes/helm-chart
 
-echo $URI $REPO $BRANCH $FILEPATH 
+#echo $URI $REPO $BRANCH $FILEPATH 
 
 # make a temporary dir
 TMPDIR="$(mktemp -d)"
@@ -19,6 +19,8 @@ cd $TMPDIR
 git init --quiet
 git remote add origin https://$AUTH@$SRV/$REPO.git
 git pull --depth=1 --quiet origin $BRANCH
+
+find ./
 
 if [ -f $FILEPATH ]; then # if a file named $FILEPATH exists
   cat $FILEPATH
